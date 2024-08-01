6021361e4b412dc7fb42b65c5680c24f17ef6890
sean.wilkinson <sean.wilkinson2000@gmail.com>
2024-08-01 14:57:19 +0100

    Cucumber Tests

--- src/test/java/stepdefinitions/NegativeCheckout.java
+++ src/test/java/stepdefinitions/NegativeCheckout.java
@@ -42,7 +42,7 @@ public class NegativeCheckout {
     }
 
     @Given("I am on the current home page 1")
-    public void i_am_on_the_current_home_page_1() {
+    public void i_am_on_the_current_home_page() {
 
         WebDriverManager.chromedriver().setup();
 
@@ -56,26 +56,26 @@ public class NegativeCheckout {
     }
 
     @When("I click on desired product type 1")
-    public void i_click_on_desired_product_type_1() {
+    public void i_click_on_desired_product_type() {
         findAndClick(By.cssSelector("a[href=\"#\"][onclick=\"byCat('notebook')\"]"));
 
     }
 
     @And("I click on desired laptop 1")
-    public void I_click_on_desired_laptop_1() {
+    public void I_click_on_desired_laptop() {
         WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='tbodyid']/div[1]/div/div/h4/a[text()='" + "Sony vaio i5" + "']")));
         findAndClick(By.cssSelector("#tbodyid > div:nth-child(1) > div > div > h4 > a"));
 
     }
 
     @And("I click add to cart 1")
-    public void i_click_add_to_cart_1() {
+    public void i_click_add_to_cart() {
         findAndClick(By.cssSelector("#tbodyid > div.row > div > a"));
 
     }
 
     @And("I navigate to the basket 1")
-    public void i_navigate_to_the_basket_1() {
+    public void i_navigate_to_the_basket() {
         findAndClick(By.cssSelector("#cartur"));
 
     }
@@ -88,22 +88,22 @@ public class NegativeCheckout {
 
     @And("I enter invalid checkout details")
     public void i_enter_invalid_checkout_details(DataTable dataTable) {
-        List<Map<String, String>> details = dataTable.asMaps(String.class, String.class);
+        List<Map<String, String>> data = dataTable.asMaps(String.class, String.class);
 
-        for (Map<String, String> detail : details) {
-            String name = detail.get("Name");
-            String country = detail.get("Country");
-            String city = detail.get("City");
-            String cardNumber = detail.get("Card Number");
-            String expiryMonth = detail.get("Expiry Month");
-            String expiryYear = detail.get("Expiry Year");
+        for (Map<String, String> row : data) {
+            String name = row.get("name");
+            String country = row.get("country");
+            String city = row.get("city");
+            String card = row.get("card");
+            String month = row.get("month");
+            String year = row.get("year");
 
-            findAndType(By.cssSelector("#name"), name);
-            findAndType(By.cssSelector("#country"), country);
-            findAndType(By.cssSelector("#city"), city);
-            findAndType(By.cssSelector("#card"), cardNumber);
-            findAndType(By.cssSelector("#month"), expiryMonth);
-            findAndType(By.cssSelector("#year"), expiryYear);
+            findAndType(By.cssSelector("#name"), name != null ? name : "");
+            findAndType(By.cssSelector("#country"), country != null ? country : "");
+            findAndType(By.cssSelector("#city"), city != null ? city : "");
+            findAndType(By.cssSelector("#card"), card != null ? card : "");
+            findAndType(By.cssSelector("#month"), month != null ? month : "");
+            findAndType(By.cssSelector("#year"), year != null ? year : "");
         }
     }
 
